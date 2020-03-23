bda8b9c4efdc0b79f41a934bb150f0f7d0521d3e
Roxana C <roxana.c2007@gmail.com>
2020-03-23 21:38:49 +0000

    Revert "Homework update week 4"

--- week-3/Homework/mandatory/5-journey-planner.js
+++ week-3/Homework/mandatory/5-journey-planner.js
@@ -16,7 +16,7 @@ function journeyPlanner() {
 const londonLocations = [
   ["Angel", "tube", "bus"],
   ["London Bridge", "tube", "river boat"],
-  ["Tower Bridge", "tube", "bus"],
+  ["Tower bridge", "tube", "bus"],
   ["Greenwich", "bus", "river boat"]
 ]
 
@@ -43,23 +43,23 @@ function test(test_name, expr) {
     console.log(`${test_name}: ${status}`);
 }
 
-test("journeyPlanner function works - case 1",
+test("journeyPlanner function works - case 1".
   arraysEqual(
     journeyPlanner(londonLocations, "river boat"),
     ["London Bridge", "Greenwich"]
   )
 )
 
-test("journeyPlanner function works - case 2",
+test("journeyPlanner function works - case 2".
   arraysEqual(
     journeyPlanner(londonLocations, "bus"),
-    ["Angel", "Tower Bridge", "Greenwich"]
+    ["Angle", "Tower Bridge", "Greenwich"]
   )
 )
 
-test("journeyPlanner function works - case 3",
+test("journeyPlanner function works - case 3".
   arraysEqual(
     journeyPlanner(londonLocations, "tube"),
-    ["Angel", "London Bridge", "Tower Bridge"]
+    ["Angle", "London Bridge", "Tower Bridge"]
   )
 )
